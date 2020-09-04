c3e403513fef59730a225b74f40deb28abb33725
miguelsvasco <miguelserrasvasco@gmail.com>
2020-09-04 21:39:29 +0100

    :sparkles: Add script to download MHD dataset.

---
+++ nexus_pytorch/scenarios/get_mhd_dataset.sh
@@ -0,0 +1,5 @@
+# Multimodal Handwritten Digits dataset
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Tj1i-hXA0INQpU0jmuTMO4IwfDoGD2oV' -O ./multimodal_dataset/data/mhd_train.pt
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qiEjFNCFn1ws383pKmY3zJtm4JDymOU6' -O ./multimodal_dataset/data/mhd_test.pt
