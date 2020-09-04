e6fdf8c56c977ba6077442dbbe39eeeabb4c459d
miguelsvasco <miguelserrasvasco@gmail.com>
2020-09-04 21:50:38 +0100

    :sparkle: Add script to download pretrained evaluation models.

---
+++ nexus_pytorch/evaluation/get_pretrained_models.sh
@@ -0,0 +1,58 @@
+# Autoencoders
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1FSirDxZppZzP8S7OH1JQCBDq10VBpo3W' -O ./autoencoders/image/trained_models/best_image_ae_model.pth.tar
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1T-kOSyIFH0KCU0RPDD2Sstdk2T6R7c2h' -O ./autoencoders/sound/trained_models/best_sound_ae_model.pth.tar
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=17t02HC1ke4YmRq6WIFB5X1NLgqxwzYai' -O ./autoencoders/trajectory/trained_models/best_trajectory_ae_model.pth.tar
+
+
+# Classifiers
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1_Om2tDwb2QUw3jfYdnel3UpsJlOyDww1' -O ./classifiers/image/trained_models/best_image_classifier_model.pth.tar
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1iEjmPQrMrWQZNFUw0BDLLHIaxYoIU9g9' -O ./classifiers/sound/trained_models/best_sound_classifier_model.pth.tar
+
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KQK--Xfql9kI6ZMl7G1LkQlL6mzyM-YS' -O ./classifiers/trajectory/trained_models/best_trajectory_classifier_model.pth.tar
+
+
+# Preliminary
+
+## Dropout
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1B-txFtR5mT9Wub0vbaKd0CsrsAm1yt5c' -O ./preliminary/dropout/trained_models/nexus_checkpoint.pth.tar
+
+
+## Hierarchy
+
+### S-Nexus
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1TgPm47wRFC0T8q-eAFi1LzL0oz39a1l-' -O ./preliminary/hierarchy/s_nexus/trained_models/s_nexus_checkpoint.pth.tar
+
+### S-MVAE
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1K0inecRnbwbMrt3sZso_fripUpBHfwsr' -O ./preliminary/hierarchy/s_mvae/trained_models/s_mvae_checkpoint.pth.tar
+
+### S-MMVAE
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1FGCu805MPN_qz675T5NRk6k_rpr5TyJu' -O ./preliminary/hierarchy/s_mmvae/trained_models/s_mmvae_checkpoint.pth.tar
+
+### H-Nexus
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1O-ieEOrOH6kXTDZDewZJyoUX5uF9XSmF' -O ./preliminary/hierarchy/h_nexus/trained_models/h_nexus_checkpoint.pth.tar
+
+### H-MVAE
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1w6XaF2zijXc-AF-8DLz-EZ1dBfic7Nq0' -O ./preliminary/hierarchy/h_mvae/trained_models/h_mvae_checkpoint.pth.tar
+
+### H-MMVAE
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1KWhAi2p3kCGhFDjGelIQBEzBz0RNL7-z' -O ./preliminary/hierarchy/h_mmvae/trained_models/h_mmvae_checkpoint.pth.tar
+
+
+# Multimodal
+
+## SigmaVAE
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1wHfRBOxmgC4yEsuAIJjTI2U9C6QdnMfO' -O ./multimodal/SigmaVae/trained_models/best_sound_vae_model.pth.tar
+
+## Nexus
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1aKwiISULEUJ00p0rZGE4FQiBMdTZW9-8' -O ./multimodal/nexus/trained_models/nexus_checkpoint.pth.tar
+
+## MVAE
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=19HPCl-z7fC-CyQ41l2KCiSWCDf__8uRL' -O ./multimodal/mvae/trained_models/mvae_checkpoint.pth.tar
+
+## MMVAE
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1r-Mwn0Y7ckkN1RNVyslv-kFSI-CqUgiF' -O ./multimodal/mmvae/trained_models/mmvae_checkpoint.pth.tar
