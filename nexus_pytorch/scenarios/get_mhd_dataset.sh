89bfd2f2e6ed64937081aa49e9fc6c185c52659a
miguelsvasco <miguelserrasvasco@gmail.com>
2020-09-05 13:43:20 +0100

    :wrench: Update script to download MHD dataset.

--- nexus_pytorch/scenarios/get_mhd_dataset.sh
+++ nexus_pytorch/scenarios/get_mhd_dataset.sh
@@ -1,5 +1,6 @@
 # Multimodal Handwritten Digits dataset
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Tj1i-hXA0INQpU0jmuTMO4IwfDoGD2oV' -O ./multimodal_dataset/data/mhd_train.pt
 
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qiEjFNCFn1ws383pKmY3zJtm4JDymOU6' -O ./multimodal_dataset/data/mhd_test.pt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1Tj1i-hXA0INQpU0jmuTMO4IwfDoGD2oV' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1Tj1i-hXA0INQpU0jmuTMO4IwfDoGD2oV" -O ./multimodal_dataset/data/mhd_train.pt && rm -rf /tmp/cookies.txt
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qiEjFNCFn1ws383pKmY3zJtm4JDymOU6' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1qiEjFNCFn1ws383pKmY3zJtm4JDymOU6" -O ./multimodal_dataset/data/mhd_test.pt && rm -rf /tmp/cookies.txt
