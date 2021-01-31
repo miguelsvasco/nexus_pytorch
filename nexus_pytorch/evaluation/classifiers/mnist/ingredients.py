f98b69cfc3f83a0f15dea300c9d7ff776c6849df
miguelsvasco <miguelserrasvasco@gmail.com>
2021-01-31 20:58:47 +0000

    :sparkle: Add standard dataset evaluation auxiliary files.

---
+++ nexus_pytorch/evaluation/classifiers/mnist/ingredients.py
@@ -0,0 +1,49 @@
+import sacred
+
+###############
+#  Training   #
+###############
+
+training_ingredient = sacred.Ingredient('training')
+
+
+@training_ingredient.config
+def training_config():
+
+    experiment = 'mnist_classifier'
+
+    # Dataset parameters
+    batch_size = 64
+
+    # Training Hyperparameters
+    epochs = 100
+    learning_rate = 1e-3
+
+    # Seed
+    seed = 4
+
+
+
+########
+# CUDA #
+########
+
+gpu_ingredient = sacred.Ingredient('gpu')
+
+
+@gpu_ingredient.config
+def gpu_config():
+    cuda = True
+
+
+
+##############
+# Evaluation #
+##############
+
+evaluation_ingredient = sacred.Ingredient('evaluation')
+
+
+@evaluation_ingredient.config
+def evaluation_config():
+    file = 'best_mnist_classifier_model.pth.tar'
